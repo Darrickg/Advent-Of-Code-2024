98a8f45c9f4307df9a795db83e2e0d0712a27e71
dga48 <darrickgunawan@gmail.com>
2024-12-01 18:29:10 -0800

    added day 1 part 2

---
+++ day1_2/day1_2.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+
+bool comp(int a, int b) {
+    return a >= b;
+}
+
+int day1_2() {
+    ifstream f("day1.txt");
+
+    if (!f.is_open()) {
+        cerr << "Error opening file";
+        return 1;
+    }
+
+    int total = 0;
+    string s;
+    vector<int> first;
+    unordered_map<int, int> count;
+    string del = "   ";
+
+    while (getline(f, s)) {
+        vector<int> numbers;
+        istringstream stream(s);
+        int num;
+
+        while (stream >> num) {
+            numbers.push_back(num);
+        }
+
+        first.push_back(numbers[0]);
+        count[numbers[1]]++;
+    }
+
+    for (int i : first) {
+        if (count.find(i) != count.end()) {
+            total += i * count[i];
+        }
+    }
+
+    return total;
+}
+
+int main () {
+    cout << day1_2() << endl;
+    return 0;
+}
